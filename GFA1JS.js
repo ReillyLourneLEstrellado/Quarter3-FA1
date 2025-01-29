c9011cd7f6df228a582bd5310a8885cad9230de9
ReillyLourneLEstrellado <reillylourne.estrellado@smc.pshs.edu.ph>
2025-01-29 19:20:21 +0800

    Create GFA1JS.js
    did conversions for height and weight

---
+++ GFA1JS.js
@@ -0,0 +1,15 @@
+var nickname="Reilly";
+
+var height=70;
+
+var weight=65;
+
+
+var heightConv1=height%12;
+
+var heightConv2=(height-heightConv1)/12;
+
+var weightConv=weight*2.205;
+
+
+alert("Name: " +nickname+ "\nHeight: " +heightConv2+ "'" +heightConv1+ "\"" + "\nWeight: " +weightConv+ " lbs");

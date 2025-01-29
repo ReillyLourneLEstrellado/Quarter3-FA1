4e893b084f1b6ab68d7b796f31fbb73377edfa94
ReillyLourneLEstrellado <reillylourne.estrellado@smc.pshs.edu.ph>
2025-01-29 19:48:52 +0800

    Create GFA1JS(2).js
    Asked for consent + show info and if no consent added a text for rejection

---
+++ GFA1JS(2).js
@@ -0,0 +1,5 @@
+var Yes = confirm("Do you wish to share your personal information? ");
+if(Yes)
+    console.log("Name: " +nickname+ "\nHeight: " +heightConv2+ "'" +heightConv1+ "\"" + "\nWeight: " +weightConv+ " lbs");
+else
+    console.log("User does not wish to share his/her information.");
